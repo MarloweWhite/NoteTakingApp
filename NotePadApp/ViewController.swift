19fae6aa030946bdb420da186384141a4065d4cb
Marlowe White <marlowe_white@yahoo.co.uk>
2019-05-16 16:31:48 +0100

    initial commit

---
+++ NotePadApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotePadApp
+//
+//  Created by Marlowe White on 16/05/2019.
+//  Copyright © 2019 Marlowe White. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
