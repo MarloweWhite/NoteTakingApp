8f91e11f88517c4d7eed58955fe16ba5c670c163
Marlowe White <marlowe_white@yahoo.co.uk>
2019-05-17 18:04:37 +0100

    Added table on click features

--- NotePadApp/ViewController.swift
+++ NotePadApp/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NotePadViewController: UITableViewController {
 
+    let itemArray = ["work", "drink", "work"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "notePadItemCell", for: indexPath)
+    
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //Tableview delegate methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+                
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
 
+        
+        
+    }
 }
 
